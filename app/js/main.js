c8813374f0b1548355832dc3610069959d8bee5e
Vilde <rendow33@gmail.com>
2022-02-14 20:32:56 +0300

    second page

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,26 @@
-$(function(){
+$(function () {
+  $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    asNavFor: ".slider-dots",
+  });
 
-$('.header__slider').slick({
-	infinite:true,
-	fade:true,
-	prevArrow:'  <img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
-	nextArrow:'  <img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-	asNavFor:'.slider-dots',
-})
-
-  $('.slider-dots').slick({
-	slidesToShow: 4,
-	slidesToScroll: 4,
-	asNavFor: '.header__slider',
-  })
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider",
+  });
+  
+  $(".serf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+  });
 });

b77e8020e3ad62fd8dd4e86933b1a6f284c39a32
Vilde <rendow33@gmail.com>
2022-02-15 22:33:39 +0300

    second page comlete

--- app/js/main.js
+++ app/js/main.js
@@ -6,21 +6,29 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dotshead",
   });
 
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
   });
   
-  $(".serf-slider").slick({
+  $(".surf-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow:
       '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+      asNavFor: ".slider_map",
   });
+  $('.slider_map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows:false,
+    asNavFor: ".surf-slider",
+    focusOnSelect:true,
+  })
 });

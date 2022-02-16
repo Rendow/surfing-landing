fd2dab1678d1de4b0cb654b1770101d903ba44b0
Vilde <rendow33@gmail.com>
2022-02-16 16:23:10 +0300

    travel complete

--- app/js/main.js
+++ app/js/main.js
@@ -31,4 +31,13 @@ $(function () {
     asNavFor: ".surf-slider",
     focusOnSelect:true,
   })
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+  
+  })
 });
